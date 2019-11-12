97d433f29f4b6eed5d7012fbb48ff3b6e5611685
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-11-12 19:57:07 -0200

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,109 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Olympicus</strong>
+        <span>R$192,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
