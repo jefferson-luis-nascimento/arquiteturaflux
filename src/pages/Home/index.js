a9ff685462715c8a80bfd0e8286128739e0035d1
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-11-13 12:04:49 -0200

    Buscando produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,109 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/88285802/tenis-olympikus-wellness-masculino-img.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis Olympicus</strong>
-        <span>R$192,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+import api from '../../services/api';
+
+import { formatPrice } from '../../util/format';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get(`products`);
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
